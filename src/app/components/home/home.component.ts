da2ba6fc27892a828a6460a3dbb42eb2e1125397
thegreatyamori <jersonmorocho16@gmail.com>
2019-10-03 15:44:15 -0500

    HOME: Barra de botones

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public navImage;
+  public navCalendarImage;
+  public navDonationsImage;
+  public navHDBImage;
+  public textCaledar: String;
+  public textDonations: String;
+  public textHDB: String;
 
   constructor() { }
 
   ngOnInit() {
+    this.navImage = 'assets/img/home/bg-buttons-bar.svg'
+    this.navCalendarImage = 'assets/img/home/card-calendar.jpg'
+    this.navDonationsImage = 'assets/img/home/card-donations.jpg'
+    this.navHDBImage = 'assets/img/home/card-hdb.jpg'
+    this.textCaledar = 'Calendario'
+    this.textDonations = 'Donaciones'
+    this.textHDB = 'Hogares de Bendición'
   }
 
 }
