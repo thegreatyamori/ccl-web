06d14d442e62605d4d59acc3bf3b2d68afc93e2a
thegreatyamori <jersonmorocho16@gmail.com>
2020-09-05 15:00:14 -0500

    🎯 Mini correciones

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: true,
-  api: 'https://admin.centrocristianodeloja.org/api/v1/',
-  storage: 'https://admin.centrocristianodeloja.org/storage/',
-  radio: 'none',
-};
+         production: true,
+         api: 'https://admin.centrocristianodeloja.org/api/v1/',
+         storage: 'https://admin.centrocristianodeloja.org/storage/',
+         radio: 'none',
+       };
